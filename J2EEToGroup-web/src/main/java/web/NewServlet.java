5000c38a9feab5dbc3114ce0cc39b7bd9b5ca9a6
okrave <lomanto.calogero_luca_maria@educ.di.unito.it>
2019-07-24 21:57:46 +0200

    Pagina profilo con css, aggiungere lista gruppi utente

--- J2EEToGroup-web/src/main/java/web/NewServlet.java
+++ J2EEToGroup-web/src/main/java/web/NewServlet.java
@@ -69,7 +69,30 @@ public class NewServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
+            out.println("<nav class=\"navbar navbar-inverse \"><div class=\"container\" ><div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\"><ul class=\"nav navbar-nav navbar-right\"><form class=\"navbar-form navbar-left\"></form><li><a class=\"creaNuovoGruppo\" href=\"http://localhost:8080/newGroup\">Crea un nuovo gruppo</a></li><li ><a href=\"http://localhost:8080/esplora\">Esplora</a></li><li class=\"dropdown\" ><a class=\"dropdown-toggle\" data-toggle=\"dropdown\"><img src=\"https://source.unsplash.com/random/20x20\" class=\"profile-image img-circle\">"+userLogged.getUserName()+"</a><ul class=\"dropdown-menu dropdown-navbar\"><li><a><i class=\"fa fa-user\"></i> &nbsp  Profilo</a></li><li><a ><i class=\"fa fa-cog\"></i> &nbsp Impostazioni</a></li><li class=\"divider\"></li><li><a ><i class=\"fa fa-sign-out\"></i> &nbsp &nbsp Logout</a></li></ul></li></ul></div><!-- /.navbar-collapse --></div><!-- /.container-fluid --></nav>");
+            out.println(" <link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.5.0/css/all.css\" integrity=\"sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU\" crossorigin=\"anonymous\">");
+            //out.println("<link rel=\"stylesheet\" href=\"userPage.css\">");
             out.println("<title>Servlet NewServlet</title>");            
+            
+            out.println("<style>");
+            out.println(".user__header__name{text-align: center;margin-bottom: 20px;} .user__header>img{border-radius: 150px;height: 300px;width: 300px;box-shadow: 0 2px 10px #424242;margin-left: 36%;margin-right: 37%;margin-top: 5%;margin-bottom: 1.6%;} .fa-calendar-alt{padding-right: 4px;font-size: 20px;margin-right: 5%;font-size: medium;text-align: right;}  .fa-users{font-size: 20px;margin-right: 5%;font-size: medium;} .user__body{padding-top: 45px;} .user__header>a{margin-left: 42%;margin-right: 40%;} .user__body__description{padding: 20%;padding-top: 20px;padding-bottom: 20px;} .user__body__description>p{padding: 2%;} .descriptionInput{padding: 2%;min-height: 110px;max-height: 110px;border: 0px;} .row{margin-right: 10px; margin-left: 64px;} .navbar-inverse{margin: 0;}  .dropdown-menu{min-width: 132px;}  .navbar-nav>li>a.creaNuovoGruppo{color:white;opacity: 5%;}  .navbar-nav>li>a.creaNuovoGruppo:hover{color:dodgerblue;opacity: 5%;}  .navbar-inverse{margin: 0;} .footer{background: #333;padding: 30px;color: #fff;margin-top: 40px;}");
+            /*out.println(".user__header>img{border-radius: 150px;height: 300px;width: 300px;box-shadow: 0 2px 10px #424242;margin-left: 37.5%;margin-right: 35%;margin-top: 5%;margin-bottom: 1.6%;}" +
+                ".user__body{padding-top: 45px;}" +
+                ".user__header>a{margin-left: 42%;margin-right: 40%;}" +
+                ".user__header__name{text-align: center;margin-bottom: 20px;}" +
+                " " +
+                ".user__body__description{padding: 20%;padding-top: 20px;padding-bottom: 20px;}" +
+                ".user__body__description>p{padding: 2%;}" +
+                "" +
+                "#appUserPage{visibility: hidden;}" +
+                "" +
+                "#appUserPageJ2ee{visibility: hidden;}" +
+                ".descriptionInput{padding: 2%;min-height: 110px;max-height: 110px;border: 0px;}" +
+                "" +
+                "" +
+                ".fa-calendar-alt{padding-right: 4px;font-size: 20px;margin-right: 5%;font-size: medium;text-align: right;}" +
+                ".fa-users{font-size: 20px;margin-right: 5%;font-size: medium;}");*/
+            out.println("</style>");
             out.println("</head>");
             out.println("<body>");    
             out.println("<div id=\"appUserPage\">" +               
@@ -91,32 +114,14 @@ public class NewServlet extends HttpServlet {
                                     "</div>"+
                                 "</div>"+
                                 "<hr>"+
-                                "<div class=\"container-fluid user__body\">"+
-                                    "<div class=\"user__body__description\"><h3>Descrizione</h3>"+
-
-                                        "<textarea id=\"descriptionInput\"" +
-                                                  "cols=\"50\""+
-                                                  "rows=\"10\"" +
-                                                  "placeholder=\"Description\""+                                         
-                                                  "class=\"descriptionInput\""+
-                                                  "value=\"Piccola descrizione \""+
-                                        "></textarea>"+
-
-                                        "<button class=\"btn btn-default\" id=\"descriptionButton\" >"+
-                                            "Cambia Descrizione"+
-                                        "</button>"+
-
-                                    "</div>"+
-
-                                "</div>"+
 
                                 "<hr>"+
                                 
                             "</div>"+
                         "</div>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
-            out.println("<h1> " + userLogged.getUserName()+ "</h1>");
-            out.println("<h1> " + userLogged.getuserEmail()+ "</h1>");
+            out.println("<footer class=\"footer\"><p>Copyright 2017 Lorem ipsum dolor sit amet</p></footer>");
+            
+            out.println("         <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\" integrity=\"sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu\" crossorigin=\"anonymous\">");
             out.println("</body>");
             out.println("</html>");
             
